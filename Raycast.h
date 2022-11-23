b3e028947f4a8d2ed52db11a755e82bf00cf3521
ShibeMaster <tom.brandweiner@gmail.com>
2022-11-23 13:10:38 +1100

    Added input, raycasting and finding entities with a specific script in a scene.

---
+++ Raycast.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <glm/glm.hpp>
+#include "Console.h"
+#include "SceneManager.h"
+#include "MeshCollisionBox.h"
+#include "Engine.h"
+struct Ray {
+	glm::vec3 origin;
+	glm::vec3 direction;
+};
+
+bool IntersectsCollider(const Ray& ray, const MeshCollisionBox& collider) {
+	float intersectMinX = (collider.min.x - ray.origin.x) / ray.direction.x;
+	float intersectMaxX = (collider.max.x - ray.origin.x) / ray.direction.x;
+
+	if (intersectMinX > intersectMaxX) std::swap(intersectMinX, intersectMaxX);
+
+	float intersectMinY = (collider.min.y - ray.origin.y) / ray.direction.y;
+	float intersectMaxY = (collider.max.y - ray.origin.y) / ray.direction.y;
+
+	if (intersectMinY > intersectMaxY) std::swap(intersectMinY, intersectMaxY);
+
+
+	float tMin = std::max(intersectMinX, intersectMinY);
+	float tMax = std::min(intersectMaxX, intersectMaxY);
+
+	if (intersectMinX > intersectMaxY || intersectMinY > intersectMaxX)
+		return false;
+	
+	if (intersectMinY > intersectMinX)
+		intersectMinX = intersectMinY;
+
+
+	if (intersectMaxY < intersectMaxX)
+		intersectMaxX = intersectMaxY;
+	
+
+	float intersectMinZ = (collider.min.z - ray.origin.z) / ray.direction.z;
+	float intersectMaxZ = (collider.max.z - ray.origin.z) / ray.direction.z;
+
+	if (intersectMinZ > intersectMaxZ) std::swap(intersectMinZ, intersectMaxZ);
+
+	if (intersectMinX > intersectMaxZ || intersectMinZ < intersectMaxX)
+		return false;
+
+	if (intersectMinZ > intersectMinX)
+		intersectMinX = intersectMinZ;
+
+	if (intersectMaxZ < intersectMaxX)
+		intersectMaxX = intersectMaxZ;
+
+	return true;
+
+}
+
+bool CheckCollision(Ray& ray, std::vector<unsigned int>* outHit) {
+	for (auto collider : Engine::FindComponentsInScene<MeshCollisionBox>()) {
+		if (IntersectsCollider(ray, Engine::GetComponent<MeshCollisionBox>(collider))) {
+			Console::LogMessage("collided");
+			outHit->push_back(collider);
+		}
+	}
+	return outHit->size() > 0;
+}
+
+
+bool Raycast(glm::vec3 origin, glm::vec3 direction, float distance, std::vector<unsigned int>* outHits = nullptr) {
+	Ray ray = { origin, direction };
+	return CheckCollision(ray, outHits);
+}
